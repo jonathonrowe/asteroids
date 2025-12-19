1829b18c7100e4ea757411b8e71d7544c64780b6
jonathonrowe <jonathonlrowe@gmail.com>
2025-12-19 16:25:56 -0600

    initialized score functionality

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
 import sys
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, FONT, FONT_SIZE
 from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from shot import Shot
+from score import Score
+
+def show_score(x, y):
+
+    score_text = font.render(f"Score: {score}", True, (255, 255, 255))
 
 def main():
-        
+    # print to the terminal basic information.
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    # intitialize a pygame with a screen of desired screen width and height. Set the caption to be the title of the game.
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids by Jon")
+    # initialize a time clock. Set delta to zero.
 
     clock = pygame.time.Clock()
     dt = 0
+    # initialize a score variable set to zero.
+
+    score = Score()
+
+    font = pygame.font.SysFont(FONT, FONT_SIZE)
+    # Create groups of different categories based on functionality
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
+    # Create containers for all the categories a certain sprite falls in.
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable,)
     Shot.containers = (shots, updatable, drawable)
+    # Create the player and asteroid field.
 
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     asteroidfield = AsteroidField()
+    # MAIN GAME CODE
 
     while True:
         log_state()
@@ -56,6 +74,9 @@ def main():
         for sprite in drawable:
             sprite.draw(screen)
         
+
+        screen.blit(score_text, (15, 10))
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
