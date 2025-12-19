c101eb76a9395e41207d6f3c2bacd99328aece93
jonathonrowe <jonathonlrowe@gmail.com>
2025-12-19 10:20:08 -0600

    added circleshape.py and player.py for player functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
         
@@ -11,13 +12,22 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
